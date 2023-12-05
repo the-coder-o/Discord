809f2f1122a82f93191831127f6a77dad67995b7
Hacker00002 <abdulbositsharipovv@gmail.com>
2023-12-05 18:58:02 +0500

    finished manage members delete kick change role and others

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react'
 
 import InviteModal from '@/components/modals/invite-modal'
+import MembersModal from '@/components/modals/members-modal'
 import EditServerModal from '@/components/modals/edit-server-modal'
 import CreateServerModal from '../../components/modals/create-server-modal'
 
@@ -20,6 +21,7 @@ const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
+      <MembersModal />
     </>
   )
 }
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -41,7 +41,7 @@ const ServerHeader = ({ role, server }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className={'px-3 py-2 text-sm cursor-pointer'}>
+          <DropdownMenuItem onClick={() => onOpen('members', { server })} className={'px-3 py-2 text-sm cursor-pointer'}>
             Manage Members
             <Users className={'h-4 w-4 ml-auto'} />
           </DropdownMenuItem>
